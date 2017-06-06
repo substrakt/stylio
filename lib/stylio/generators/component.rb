b368d83a7e1cfac40e8af1701138b684d87db2df
Max Woolf <maximus@zingysaturn.co.uk>
2017-06-06 10:04:14 +0100

    FIX: Use an underscore for partial names (#15)

--- lib/stylio/generators/component.rb
+++ lib/stylio/generators/component.rb
@@ -8,18 +8,29 @@ module Stylio
         @name = name
       end
 
+      def fullpath(extension)
+        if extension == 'html.erb'
+          "components/#{name}/_#{name}.#{extension}"
+        else
+          "components/#{name}/#{name}.#{extension}"
+        end
+      end
+
       def generate
         puts "Generating component with name #{name}".yellow
         FileUtils::mkdir_p "components/#{name}"
         puts "Created folder components/#{name}".green
-        File.open("components/#{name}/#{name}.yml", 'w') do |f|
+
+        File.open(fullpath('yml'), 'w') do |f|
           f.write('')
         end
-        puts "Created file components/#{name}/#{name}.yml".green
-        File.open("components/#{name}/#{name}.html.erb", 'w') do |f|
+        puts "Created file #{fullpath('yml')}".green
+
+        File.open(fullpath('html.erb'), 'w') do |f|
           f.write('')
         end
-        puts "Created file components/#{name}/#{name}.html.erb".green
+        puts "Created file #{fullpath('html.erb')}".green
+
         puts "Generated component with name #{name}".green
       end
     end
